d66393d2af4951935764d13fe504023807662811
akashcraft <akashcraft2@gmail.com>
2024-07-17 20:53:32 -0230

    Added Power Toggle, Keybinds, Hex Colour Picker

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # LightCraft
 Software to control Bluetooth LED Strips like QHM-0A9E
 
-<img width=600px src="https://github.com/user-attachments/assets/2b842b30-9303-430c-9ac6-cfd6d8ca6d31">
+<img width=600px src="https://github.com/user-attachments/assets/7ebbe3ff-4129-43fd-b2b7-0fcd95be3e73">
 
 ## Features
-- Auto Connect
+- Connect and Power Toggle
 - Single Colour Control
+- Hex Colour Control
+- Keybind Shortcuts
 - Dark Mode
 
 ## Under Development
 Some of these features are available and ready to use in the CLI Version. Run the `main.py` file.
-- Hex Colour Control (CLI)
 - Pulsing with Timing Control (CLI)
 - Flashing with Timing Control (CLI)
 - Music Beats Sync (CLI)
@@ -18,10 +19,11 @@ Some of these features are available and ready to use in the CLI Version. Run th
 - Multi-Language Support (Planned)
 
 ## Installation
-There are no available release as of now but you may choose to run the source code yourself, Run the `lightcraft.py` file. Here are the requirements:
+There are no available release as of now but you may choose to run the source code yourself, Run the `lightcraft.py` file. Please read important notes below. Here are the requirements:
 
 - Python [Get it here](https://www.python.org/downloads/release/)
 - customtkinter [Get it here](https://github.com/TomSchimansky/CustomTkinter)
+- CTkColorPicker [Get it here](https://github.com/Akascape/CTkColorPicker)
 - bleak [Get it here](https://github.com/hbldh/bleak)
 - pillow
   
@@ -31,7 +33,7 @@ pip install -r requirements.txt
 ```
 or simply paste the following in terminal
 ```
-pip install customtkinter, bleak, pillow
+pip install customtkinter, CTkColorPicker, bleak, pillow
 ```
 Clone this Repository by
 ```
@@ -39,7 +41,12 @@ git clone https://akashcraft/LED-Controller.git
 ```
 
 ## Important Notes
-LightCraft CLI version will not be developed further. Use it for a light weight option if you just want to send custom commands to your LED Strips.
+LightCraft CLI
+This version will not be developed further. Use it for a light weight option if you just want to send custom commands to your LED Strips.
+
+LightCraft GUI
+If you plan to run the project locally, you will need to modify the `CTkColorPicker` package as the stock package is modified to add functionality to LightCraft. Find the file in the repository and overwrite with the stock file. Failure to do this will result in Runtime Errors.
+
 LightCraft stores its configuration data in the `Settings.txt` which must be located in the same project folder. If this is not possible, LightCraft will attempt to re-create the files during the pre-GUI checks. Resources folder contains all the GUI elements and this folder **must not** be deleted. LightCraft will not load the GUI in that case.
 
 ## User Manual
--- lightcraft.py
+++ lightcraft.py
@@ -6,10 +6,14 @@ from bleak import BleakClient
 from PIL import Image
 from customtkinter import *
 import tkinter as tk
+from CTkColorPicker import *
+from functools import wraps
 
 root = CTk()
 address = "32:06:C2:00:0A:9E"
 char_uuid = "FFD9"
+isConnected = False
+isOn = False
 
 validColours = {
     'red': [255, 0, 0],
@@ -65,15 +69,36 @@ class BluetoothController:
         return asyncio.run_coroutine_threadsafe(coro, self.loop)
 
 def main():
+    def debounce(wait):
+        def decorator(fn):
+            last_call = [0]
+
+            @wraps(fn)
+            def debounced(*args, **kwargs):
+                now = time.time()
+                if now - last_call[0] >= wait:
+                    last_call[0] = now
+                    return fn(*args, **kwargs)
+            return debounced
+        return decorator
+    
     def connect():
-        future = controller.run_coroutine(controller.connect())
-        connect_button.configure(text="Connecting", state="disabled",fg_color=("#3b8ed0","#1f6aa5"),hover_color=("#36719f","#144870"))
-        root.after(20, lambda: check_connection(future))
+        global isConnected
+        if isConnected:
+            isConnected = False
+            connect_button.configure(text="Connect", state="normal",fg_color=("#3b8ed0","#1f6aa5"),hover_color=("#36719f","#144870"))
+            disconnect()
+        else:
+            future = controller.run_coroutine(controller.connect())
+            connect_button.configure(text="Connecting", state="disabled",fg_color=("#3b8ed0","#1f6aa5"),hover_color=("#36719f","#144870"))
+            root.after(20, lambda: check_connection(future))
 
     def check_connection(future):
+        global isConnected
         if future.done():
             if controller.connected:
-                connect_button.configure(text="Connected", fg_color="green", hover_color="#00AA00", state="disabled")
+                isConnected = True
+                connect_button.configure(text="Connected", fg_color="green", hover_color="#00AA00", state="normal")
             else:
                 connect_button.configure(text="Reconnect", fg_color="red", hover_color="#AA0000", state="normal")
                 tk.messagebox.showerror("Connection Failure", "LightCraft failed to connect with your LED Strips. Please make sure that your Bluetooth is turned on and that your LED Strips are not bonded with another device. Verify the MAC Address in Settings.")
@@ -83,13 +108,46 @@ def main():
     def disconnect():
         controller.run_coroutine(controller.disconnect())
 
+    def togglePower():
+        global isOn
+        if not isOn:
+            isOn = True
+            power_button.configure(image=imgtk2)
+            data = bytearray([0xcc,0x23,0x33])
+        else:
+            isOn = False
+            power_button.configure(image=imgtk3)
+            data = bytearray([0xcc,0x24,0x33])
+        controller.run_coroutine(controller.sendCmd(data))
+
+    def setBrightness(isUp):
+        curr_value = colorpicker.slider.get()
+        if isUp:
+            new_value = curr_value + 10
+            if new_value > 255:
+                new_value = 255
+        else:
+            new_value = curr_value - 10
+            if new_value < 0:
+                new_value = 0
+        colorpicker.slider.set(new_value)
+        colorpicker.update_colors()
+        sendHex(colorpicker.label.cget("text"))
+
+    @debounce(0.1)
+    def sendHex(data):
+        data = data[1:]
+        data = bytearray([0x56, int(data[0:2], 16), int(data[2:4], 16), int(data[4:6], 16), 0x00, 0xf0, 0xaa])
+        controller.run_coroutine(controller.sendCmd(data))
+
     def sendColour(r,g,b):
+        colorpicker.update_colors(r,g,b)
         data = bytearray([0x56, r, g, b, 0x00, 0xf0, 0xaa])
         controller.run_coroutine(controller.sendCmd(data))
     
     def sgButton(frame,row,col,colour):
         r, g, b = map(int, validColours[colour])
-        return CTkButton(frame,text="", fg_color="#{:02x}{:02x}{:02x}".format(r, g, b), hover=False, font=CTkFont(size=bsize), width=sgwidth, corner_radius=sgradius, height=bheight, command=lambda: sendColour(r,g,b)).grid(row=row,column=col,padx=(10,0),pady=(10,0))
+        return CTkButton(frame,text="", fg_color="#{:02x}{:02x}{:02x}".format(r, g, b), hover=False, font=CTkFont(size=bsize), width=sgwidth, corner_radius=sgradius, height=sgheight, command=lambda: sendColour(r,g,b)).grid(row=row,column=col,padx=(10,0),pady=(10,0))
 
     def destroyer(relaunch=False):
         global root
@@ -124,11 +182,15 @@ def main():
     userwiny = root.winfo_screenheight()
     x = (userwinx)//3
     y = (userwiny)//3
-    root.geometry(f"700x400+{x}+{y}")
+    root.geometry(f"700x410+{x}+{y}")
+    root.resizable(False,False)
 
     #Frames
-    basicframe=CTkFrame(root)
-    sgframe=CTkFrame(basicframe, fg_color="transparent")
+    mainframe=CTkTabview(root)
+    mainframe.add("Control")
+    mainframe.add("Music")
+    mainframe.add("Settings")
+    sgframe=CTkFrame(mainframe.tab("Control"), fg_color="transparent")
 
     #Button Scaling
     bsize=16
@@ -141,26 +203,33 @@ def main():
     #Images
     try:
         image1 = Image.open(r".\Resources\logo.png")
+        image2 = Image.open(r".\Resources\onButton.png")
+        image3 = Image.open(r".\Resources\offButton.png")
     except:
         tk.messagebox.showerror("Missing Resources","LightCraft could not find critical resources. The Resources folder may have been corrupted or deleted. Please re-install LightCraft from official sources.") #Missing Resources
         quit()
     imgtk1 = CTkImage(light_image=image1,size=(60,60))
+    imgtk2 = CTkImage(light_image=image2,size=(25,25))
+    imgtk3 = CTkImage(light_image=image3,size=(25,25))
 
     #Basic Elements
     headinglogo = CTkButton(root, text="", width=80, image=imgtk1,command=lambda :webbrowser.open("https://github.com/akashcraft/LED-Controller"),hover=False, fg_color="transparent")
     heading1 = CTkLabel(root, text="LightCraft", font=CTkFont(size=30)) #LightCraft
-    heading2 = CTkLabel(root, text="Version 1.0.1 (Beta)", font=CTkFont(size=13)) #Version
+    heading2 = CTkLabel(root, text="Version 1.0.2 (Beta)", font=CTkFont(size=13)) #Version
     connect_button = CTkButton(root, text="Connect", font=CTkFont(size=bsize), width=bwidth, height=bheight, command=connect)
+    power_button = CTkButton(root, text="", fg_color="#333333", image=imgtk3, hover=False, font=CTkFont(size=bsize), width=sgwidth, corner_radius=10, height=bheight, command=togglePower)
+    colorpicker = CTkColorPicker(mainframe.tab("Control"), width=250, orientation=HORIZONTAL, command=lambda e: sendHex(e))
 
     root.grid_columnconfigure(1,weight=1)
     root.grid_rowconfigure(2,weight=1)
     headinglogo.grid(row=0,column=0,rowspan=2,pady=10, sticky='w')
     heading1.grid(row=0,column=1,pady=0, sticky='sw')
     heading2.grid(row=1,column=1,pady=0, sticky='nw')
-    connect_button.grid(row=0,column=2,rowspan=2,padx=10, sticky='e')
-    basicframe.grid(row=2,column=0,columnspan=3,padx=10, pady=10, sticky='nsew')
-    basicframe.grid_columnconfigure(1,weight=1)
-    sgframe.grid(row=0,column=0,padx=10,pady=10)
+    connect_button.grid(row=0,column=2,rowspan=2,padx=0, sticky='e')
+    power_button.grid(row=0,column=3,rowspan=2,padx=10, sticky='e')
+    mainframe.grid(row=2,column=0,columnspan=4,padx=10, pady=(0,10), sticky='nsew')
+    mainframe.tab("Control").grid_columnconfigure(2,weight=1)
+    sgframe.grid(row=0,column=0,padx=10,pady=10, sticky='n')
     sgButton(sgframe, 0, 0, "red")
     sgButton(sgframe, 0, 1, "green")
     sgButton(sgframe, 0, 2, "blue")
@@ -181,6 +250,18 @@ def main():
     sgButton(sgframe, 4, 1, "cyan")
     sgButton(sgframe, 4, 2, "violet")
     sgButton(sgframe, 4, 3, "white")
+    colorpicker.grid(row=0,column=1,padx=42,pady=10,sticky='n')
+
+    #Key Binds
+    root.bind("<KeyRelease-r>",lambda e:sendColour(255,0,0))
+    root.bind("<KeyRelease-g>",lambda e:sendColour(0,255,0))
+    root.bind("<KeyRelease-b>",lambda e:sendColour(0,0,255))
+    root.bind("<KeyRelease-w>",lambda e:sendColour(255,255,255))
+    root.bind("<KeyRelease-p>",lambda e:sendColour(255,0,40))
+    root.bind("<KeyRelease-c>",lambda e:connect())
+    root.bind("<space>",lambda e:togglePower())
+    root.bind("<Up>",lambda e:setBrightness(True))
+    root.bind("<Down>",lambda e:setBrightness(False))
 
     #TO DO Settings Auto Reconnect
     #root.after(20,connect)
